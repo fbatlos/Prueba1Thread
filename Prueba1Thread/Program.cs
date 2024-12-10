f167073ddbe2a04d424e2ab50ef622f7b56d516e
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2024-12-10 21:14:18 +0100

    Initial commit

---
+++ Prueba1Thread/Program.cs
@@ -0,0 +1,32 @@
+﻿class ThreadTest 
+{ 
+    static void Main()  
+    { 
+        Thread t = new Thread (new ThreadStart (choseHand));
+        Thread t2 = new Thread (new ThreadStart (choseHand));
+ 
+        t.Start();
+        t2.Start();
+       
+    } 
+ 
+    static void choseHand() 
+    { 
+        int handNumber = new Random().Next(1,4);
+        String hand = null;
+        switch (handNumber)
+        {
+            case 1:
+                hand = "Piedra";
+                break;
+            case 2 :
+                hand = "Papel";
+                break;
+            case 3 :
+                hand = "Tigeras";
+                break;
+        }
+        
+        Console.WriteLine(hand);
+    } 
+}
